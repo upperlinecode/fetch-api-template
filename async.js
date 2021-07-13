c3a073b6dbd7b4273c1bc0d60ee07433d9470776
Melanie <46007892+melwritescode@users.noreply.github.com>
2021-07-13 15:06:36 -0600

    Change variable name

--- async.js
+++ async.js
@@ -12,8 +12,8 @@ const YOUR_API_KEY_HERE = 'Replace this string with your API key'
 
 // Go to https://developers.giphy.com/ and create an account. Then create your first app called testApp to create an API key. Enter it below.
 function sendApiRequest() {
-  let promise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${YOUR_API_KEY_HERE}&q=Ryan+Gosling&limit=25&offset=0&rating=G&lang=en`);
-  console.log(promise);
+  let response = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${YOUR_API_KEY_HERE}&q=Ryan+Gosling&limit=25&offset=0&rating=G&lang=en`);
+  console.log(response);
 };
 
 // querySelect the wrapper, and add an image tag to it. Interpolate the URL string from the previous function.
